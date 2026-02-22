fe1bcb519b82e14b1e47c67b6768114d4e95e151
Anson Lee <leehosanganson@gmail.com>
2026-02-22 00:24:27 +0000

    Generation 17: 2026-02-22 00:24:27

---
+++ backup-gtk.sh
@@ -0,0 +1,6 @@
+mkdir -p ~/config-backup
+mv ~/.config/gtk-4.0/settings.ini ~/config-backup/
+mv ~/.config/gtk-4.0/gtk.css ~/config-backup/
+mv ~/.config/gtk-3.0/settings.ini ~/config-backup/
+mv ~/.config/gtk-3.0/gtk.css ~/config-backup/
+mv ~/.gtkrc-2.0 ~/config-backup/
