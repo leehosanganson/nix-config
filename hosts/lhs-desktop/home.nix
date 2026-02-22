061d69bf84dba043f3caf542fae0ef9bdd2583b5
Anson Lee <leehosanganson@gmail.com>
2026-02-22 00:02:27 +0000

    Generation 12: 2026-02-22 00:02:27

--- hosts/lhs-desktop/home.nix
+++ hosts/lhs-desktop/home.nix
@@ -5,6 +5,10 @@
   home.homeDirectory = "/home/ansonlee";
   home.stateVersion = "25.11";
 
+  imports = [
+    ../../modules/theme/zen.nix
+  ];
+
   home.packages = with pkgs;
     [
       # Compilers / Interpreters
