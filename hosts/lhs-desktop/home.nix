e7b898b92c7cf762bd09e5fa11843ee26cc7f553
Anson Lee <leehosanganson@gmail.com>
2026-02-24 23:52:30 +0000

    Generation 117: 2026-02-24 23:52:30

--- hosts/lhs-desktop/home.nix
+++ hosts/lhs-desktop/home.nix
@@ -1,26 +1,23 @@
 { pkgs, inputs, ... }:
 
 {
+  home.stateVersion = "25.11";
   programs.home-manager.enable = true;
 
   home.username = "ansonlee";
   home.homeDirectory = "/home/ansonlee";
-  home.stateVersion = "25.11";
 
   imports = [
-    ../../modules/home/dotfiles.nix
-    ../../modules/home/neovim.nix
-    ../../modules/home/shell.nix
+    ../../modules/home/core
     ./variables.nix
     ./secrets.nix
 
-    # if hyprland
-    ../../modules/home/hyprland
-    ../../modules/home/hyprland-gui.nix
+    # Desktop
+    ../../modules/home/desktop/hyprland
+    ../../modules/home/gui/hyprland-gui.nix
   ];
 
   home.packages = with pkgs; [
-    # Applciations
     google-chrome
     discord
     moonlight-qt
---
+++ modules/home/core/default.nix
@@ -0,0 +1,7 @@
+_: {
+  imports = [
+    ./dotfiles.nix
+    ./neovim.nix
+    ./shell.nix
+  ];
+}
