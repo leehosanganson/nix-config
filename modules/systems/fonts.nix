9ee15e0b121c850b6d835b23f284218b7426472d
Anson Lee <leehosanganson@gmail.com>
2026-02-24 19:49:24 +0000

    Generation 100: 2026-02-24 19:49:24

--- modules/systems/fonts.nix
+++ modules/systems/fonts.nix
@@ -1,8 +1,8 @@
-_: {
-  fonts.packages = [
-    "nerd-fonts-jetbrains-mono"
-    "nerd-fonts-hack"
-    "fira-code"
+{ pkgs, ... }: {
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.hack
+    nerd-fonts.fira-code
   ];
 
   fonts.enableDefaultFonts = false;
