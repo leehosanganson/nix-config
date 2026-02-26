91920b2edfc99f0b0e8eaaf4849f6f15959d4e04
Anson Lee <leehosanganson@gmail.com>
2026-02-26 23:45:18 +0000

    Generation 142: 2026-02-26 23:45:18

--- modules/ime/default.nix
+++ modules/ime/default.nix
@@ -2,21 +2,17 @@
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.inputMethod = {
-    enabled = "fcitx5";
+    enable = true;
+    type = "fcitx5";
     fcitx5.addons = with pkgs; [
       fcitx5-gtk
-      qt6Packages.fcitx5-configtool
+      kdePackages.fcitx5-configtool
     ];
   };
 
-  # Essential environment variables for Wayland/Hyprland
-  # environment.sessionVariables = {
-  #   GTK_IM_MODULE = "fcitx5";
-  #   QT_IM_MODULE = "fcitx5";
-  #   XMODIFIERS = "@im=fcitx5";
-  #
-  #   INPUT_METHOD = "fcitx5";
-  #   SDL_IM_MODULE = "fcitx5";
-  #   GLFW_IM_MODULE = "ibus"; # Some apps prefer this for compatibility
-  # };
+  environment.sessionVariables = {
+    INPUT_METHOD = "fcitx";
+    SDL_IM_MODULE = "fcitx";
+    GLFW_IM_MODULE = "ibus";
+  };
 }
--- modules/ime/zh.nix
+++ modules/ime/zh.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: {
   i18n.inputMethod.fcitx5.addons = with pkgs; [
-    qt6Packages.fcitx5-chinese-addons
+    kdePackages.fcitx5-chinese-addons
     fcitx5-lua
   ];
 }
