280cf304ee4b41bedefbcbf912f50bc8e298e891
Anson Lee <leehosanganson@gmail.com>
2026-02-26 23:11:10 +0000

    Generation 140: 2026-02-26 23:11:10

--- modules/home/apps/yazi.nix
+++ modules/home/apps/yazi.nix
@@ -6,10 +6,14 @@ _: {
   };
 
   programs.yazi.settings = {
-    image_filter = "lanczos";
-    image_quality = "90";
-    show_hidden = true;
-    show_symlink = true;
-    sort_dir_first = true;
+    mgr = {
+      show_hidden = true;
+      show_symlink = true;
+      sort_dir_first = true;
+    };
+    preview = {
+      image_filter = "lanczos";
+      image_quality = "90";
+    };
   };
 }
