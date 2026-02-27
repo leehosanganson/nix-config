a435d0ce50c3862e3eb9126aa5e3b9415be631a0
Anson Lee <leehosanganson@gmail.com>
2026-02-27 00:50:59 +0000

    Generation 159: 2026-02-27 00:50:59

--- modules/ime/zh.nix
+++ modules/ime/zh.nix
@@ -1,9 +1,7 @@
 { pkgs, ... }: {
   i18n.inputMethod.fcitx5.addons = with pkgs; [
     kdePackages.fcitx5-chinese-addons
-    fcitx5-lua
     fcitx5-rime
     rime-data
-    fcitx5-table-extra
   ];
 }
