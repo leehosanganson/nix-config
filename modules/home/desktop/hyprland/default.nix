0b0d88ce04ed7b96b0af7f21cd0a27f62bdd14b7
Anson Lee <leehosanganson@gmail.com>
2026-02-27 00:30:05 +0000

    Generation 149: 2026-02-27 00:30:05

--- modules/home/desktop/hyprland/default.nix
+++ modules/home/desktop/hyprland/default.nix
@@ -52,9 +52,6 @@
         "XMODIFIERS,@im=fcitx"
         "GTK_IM_MODULE,fcitx"
         "QT_IM_MODULE,fcitx"
-        "GLFW_IM_MODULE,fcitx"
-        "SDL_IM_MODULE,fcitx"
-        "INPUT_METHOD,fcitx"
       ];
 
 
