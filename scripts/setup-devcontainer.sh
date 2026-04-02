58e55d0b36dace35cc56e9b82a87931e2f4b930f
Anson Lee <leehosanganson@gmail.com>
2026-04-02 22:31:37 +0100

    devcontainer script to source after install

--- scripts/setup-devcontainer.sh
+++ scripts/setup-devcontainer.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -e
+set -euo pipefail
 
 # Set up SSH key for GitHub
 if [ -n "$DEV_SSH_KEY" ]; then
@@ -20,7 +20,24 @@ fi
 # Install nix & Run home-manager
 if ! command -v nix &> /dev/null; then
     curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux --no-confirm
-    [ -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ] && . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+    if [ -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]; then
+    . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+    fi
+    if [ -e /nix/var/nix/profiles/default/etc/profile.d/nix.sh ]; then
+        . /nix/var/nix/profiles/default/etc/profile.d/nix.sh
+    fi
 fi
 
 nix run github:nix-community/home-manager -- switch -b backup --flake '.#vscode@devcontainer'
+
+# Make Home Manager’s PATH globally available in zsh
+ZSHRC="$HOME/.zshrc"
+{
+    echo ""
+    echo "# Add Home Manager user profile to PATH"
+    echo "export PATH=\"\$HOME/.nix-profile/bin:\$PATH\""
+    echo ""
+    echo "if [[ -f \"\$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh\" ]]; then"
+    echo "  source \"\$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh\""
+    echo "fi"
+} >> "$ZSHRC"
