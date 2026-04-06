271ce3de23b148e4d32d5c76b44e1e9a9ef9ebbe
Anson Lee <leehosanganson@gmail.com>
2026-04-06 16:51:11 +0100

    Generation 249: 2026-04-06 16:51:11

--- hosts/lhs-desktop/default.nix
+++ hosts/lhs-desktop/default.nix
@@ -101,4 +101,11 @@
     LC_TELEPHONE = "en_GB.UTF-8";
     LC_TIME = "en_GB.UTF-8";
   };
+
+  # Virtualisation
+  virtualisation.docker = {
+    enable = true;
+    rootless.enable = true;
+    rootless.setSocketVariable = true;
+  };
 }
