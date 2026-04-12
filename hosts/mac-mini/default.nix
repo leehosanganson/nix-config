90d2a75e367d6379ba9b3583ff21a5f9cfce91e7
Anson Lee <leehosanganson@gmail.com>
2026-04-12 14:13:41 +0100

    mac-mini OSX Generation 7: 2026-04-12 14:13:41

--- hosts/mac-mini/default.nix
+++ hosts/mac-mini/default.nix
@@ -16,9 +16,8 @@
     description = "Anson Lee";
     home = "/Users/ansonlee";
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII3aC4Qyn+J/JCcij7OU2+t5HrfqQGfVxfyQgsiDDzYe"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICZCH1vjRkNiw4b8btoOqd9WdPrN3lRc1lCrF3fWzfb+ leehosanganson@gmail.com"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB0LDIi3yFdEGJwy3ZvyXqwEMHA8bfbif7ZXZzGyM/og"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFOuRvc3yYsvjGSLlvtiSTGYx8YscOGAxuLoQEgP/llb leehosanganson@gmail.com"
     ];
   };
 
