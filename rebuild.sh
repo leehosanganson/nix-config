af580051f5a19fad3ea6bfb71235333723e0f3be
Anson Lee <leehosanganson@gmail.com>
2026-04-12 13:29:05 +0100

    mac-mini OSX Generation /run/current-system/sw/bin/darwin-rebuild:: 2026-04-12 13:29:05

--- rebuild.sh
+++ rebuild.sh
@@ -27,7 +27,7 @@ git add .
 # Rebuild
 if [[ "$(uname)" == "Darwin" ]]; then
   if sudo darwin-rebuild switch --flake .#mac-mini; then
-    gen=$(darwin-rebuild list-generations | grep True | awk '{print $1}')
+    gen=$(sudo darwin-rebuild list-generations | grep current | awk '{print $1}')
     git commit -m "$HOSTNAME OSX Generation $gen: $(date +'%Y-%m-%d %H:%M:%S')"
     git push origin main
     echo "$HOSTNAME rebuilt - gen $gen"
