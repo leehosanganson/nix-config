860f4f7e0682af814d13764f3f7ce54120317fe5
Anson Lee <leehosanganson@gmail.com>
2026-04-12 13:42:04 +0100

    mac-mini OSX Generation 5: 2026-04-12 13:42:04

--- hosts/mac-mini/default.nix
+++ hosts/mac-mini/default.nix
@@ -8,13 +8,17 @@
     ../../modules/common.nix
   ];
 
+  # ssh
+  services.openssh.enable = true;
+
   # User
   users.users.ansonlee = {
     description = "Anson Lee";
     home = "/Users/ansonlee";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII3aC4Qyn+J/JCcij7OU2+t5HrfqQGfVxfyQgsiDDzYe"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICZCH1vjRkNiw4b8btoOqd9WdPrN3lRc1lCrF3fWzfb+ leehosanganson@gmail.com"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB0LDIi3yFdEGJwy3ZvyXqwEMHA8bfbif7ZXZzGyM/og"
+    ];
   };
-
-
-  # ssh
-  services.openssh.enable = true;
 }
--- hosts/mac-mini/home.nix
+++ hosts/mac-mini/home.nix
@@ -14,10 +14,4 @@
     ../../modules/home/apps/dev.nix
     ../../modules/home/core
   ];
-
-  home.file.".ssh/authorized_keys".text = ''
-    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII3aC4Qyn+J/JCcij7OU2+t5HrfqQGfVxfyQgsiDDzYe
-    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICZCH1vjRkNiw4b8btoOqd9WdPrN3lRc1lCrF3fWzfb+ leehosanganson@gmail.com
-    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB0LDIi3yFdEGJwy3ZvyXqwEMHA8bfbif7ZXZzGyM/og
-  '';
 }
