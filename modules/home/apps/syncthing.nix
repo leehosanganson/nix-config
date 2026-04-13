c8a67d8bf4d1e4046d26c82c72d434ccf1eea4e2
Anson Lee <leehosanganson@gmail.com>
2026-04-13 22:10:55 +0100

    lhs-desktop NixOs Generation 256: 2026-04-13 22:10:55

--- modules/home/apps/syncthing.nix
+++ modules/home/apps/syncthing.nix
@@ -5,7 +5,7 @@
     settings = {
       gui = {
         user = "ansonlee";
-        password = "${config.sops.secrets.fileshare-key.path}";
+        passwordFile = config.sops.secrets."fileshare-key".path;
       };
       devices = {
         "syncthing-k3s" = { id = "54BBHO4-O7R2VDC-P7AETXN-5KS3G2Q-NURAS7S-EIU5GJL-ZTLCA67-RMESMAU"; };
