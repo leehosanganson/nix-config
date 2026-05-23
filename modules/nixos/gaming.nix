936e17f3ed131900dafe6ad1e3a4b9410a599bc2
Anson Lee <leehosanganson@gmail.com>
2026-05-23 20:41:15 +0100

    lhs-desktop NixOs Generation 288: 2026-05-23 20:41:15

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -1,7 +1,12 @@
 _: {
   services.udev.extraRules = ''
-    KERNEL=="uinput", MODE="0660", GROUP="input", SYMLINK+="uinput"
+    # 2.4GHz/Dongle
+    KERNEL=="hidraw*", ATTRS{idProduct}=="6012", ATTRS{idVendor}=="2dc8", MODE="0660", TAG+="uaccess", GROUP="input"
+
+    # Bluetooth
+    KERNEL=="hidraw*", KERNELS=="*2DC8:6012*", MODE="0660", TAG+="uaccess", GROUP="input"
   '';
+
   users.users.ansonlee.extraGroups = [ "input" ];
 
   # mouse
